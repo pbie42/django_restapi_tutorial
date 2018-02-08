a1d0abb38ffde82534bc8e2107a679ccbe5845b3
Paul BIE <pbie@e2r10p6.42.fr>
2018-02-08 20:08:59 +0100

    [Add + Wip] - First half of rest api tutorial with user authentication. Next up is using tokens

---
+++ djangorest/api/views.py
@@ -0,0 +1,22 @@
+from rest_framework import generics
+from rest_framework import permissions
+from .permissions import IsOwner
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
+
+class CreateView(generics.ListCreateAPIView):
+	"""This class defines the create behavior of our rest api."""
+	queryset = Bucketlist.objects.all()
+	serializer_class = BucketlistSerializer
+	permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+	def perform_create(self, serializer):
+		"""Save the post data when creating a new bucketlist."""
+		serializer.save(owner=self.request.user)
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+	"""This class handles the http GET(one), PUT, PATCH, and DELETE requests."""
+
+	queryset = Bucketlist.objects.all()
+	serializer_class = BucketlistSerializer
+	permission_classes = (permissions.IsAuthenticated, IsOwner)
